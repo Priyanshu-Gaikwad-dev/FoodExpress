1d9290977c7a612851e07bfc4d3816807c2b5edc
Pgaikwad203 <priyanshugaikwad30@gmail.com>
2025-06-02 10:24:27 +0530

    add dist

--- dist/assets/Grocery-DeziFm8I.js
+++
@@ -1,1 +0,0 @@
-import{j as r,H as e}from"./index-DyyZn-Yg.js";const n=()=>r.jsxs("div",{children:[r.jsx(e,{}),r.jsx("h1",{children:" Grocery component along with differnt child component"})]});export{n as default};
