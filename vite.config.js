8f6a00326a51edccfa9f2a04c8c68cfdaa6be43e
Pgaikwad203 <priyanshugaikwad30@gmail.com>
2025-06-02 11:03:41 +0530

    error fix

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,44 @@
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default {
+export default defineConfig({
   plugins: [react()],
+  
+  server: {
+    host: true,
+    port: 5173,
+    strictPort: true,
+    allowedHosts: ['e4hb6q-ip-103-56-197-29.tunnelmole.net'],
+    cors: {
+      origin: '*',
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      credentials: true
+    },
+    proxy: {
+      '/api': {
+        target: 'http://e4hb6q-ip-103-56-197-29.tunnelmole.net',
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+        configure: (proxy, _options) => {
+          proxy.on('error', (err, _req, _res) => {
+            console.log('proxy error', err);
+          });
+          proxy.on('proxyReq', (proxyReq, req, _res) => {
+            console.log('Sending Request:', req.method, req.url);
+          });
+          proxy.on('proxyRes', (proxyRes, req, _res) => {
+            console.log('Received Response:', proxyRes.statusCode, req.url);
+          });
+        }
+      }
+    }
+  },
 
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './src/setupTests.js', // or your path
-  },
-  
-};
+    setupFiles: './src/setupTests.js',
+  }
+});
